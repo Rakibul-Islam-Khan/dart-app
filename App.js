7f2ea3717bb8f74c4816c6566a46249259e0b191
Rakibul Islam Khan <62658122+Rakibul-Islam-Khan@users.noreply.github.com>
2021-11-25 21:06:42 +0600

    new

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import One from './src/components/One';
+import { One } from './assets/Components/One';
 
 export default function App() {
   return (
